fca86e29c73a871453734626eac9da46d8914a64
siddharth gupta <gsiddharth79@gmail.com>
2019-08-03 00:35:48 +0530

    added new activity(notification detail)

--- app/src/main/java/com/example/hosel_app/adapter.java
+++ app/src/main/java/com/example/hosel_app/adapter.java
@@ -3,10 +3,12 @@ package com.example.hosel_app;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +18,7 @@ import java.util.ArrayList;
 
 import static android.support.v7.widget.RecyclerView.*;
 
-public class adapter extends RecyclerView.Adapter<adapter.viewholder>implements View.OnClickListener,View.OnLongClickListener{
+public class adapter extends RecyclerView.Adapter<adapter.viewholder>{
     private ArrayList<node>ls;// the arrayllist from node
     private Context context;
     public adapter(ArrayList<node> ls){
@@ -24,28 +26,22 @@ public class adapter extends RecyclerView.Adapter<adapter.viewholder>implements 
 
     }//constructor
 
-    @Override
-    public void onClick(View view) {
-        Toast.makeText(view.getContext(), "position = 1" , Toast.LENGTH_SHORT).show();
 
-    }
+      //  Toast.makeText(view.getContext(), "position = 1" , Toast.LENGTH_SHORT).show();
 
-    @Override
-    public boolean onLongClick(View view) {
-        Toast.makeText(view.getContext(), "position = 1" , Toast.LENGTH_SHORT).show();
-        return false;
-    }
 
-    public class viewholder extends ViewHolder{//creating the viewholder class
+    public class viewholder extends ViewHolder {//creating the viewholder class
 
         public TextView textView;
         public TextView title;
         public ImageView imageView;
+        public RelativeLayout relativeLayout;
         public viewholder(@NonNull View itemView) {//constructor for the vieholder class
             super(itemView);//passing item view to the parent class
            //textView=(TextView)itemView.findViewById(R.id.txt);//taking the views from the given itemview(parameters)
             imageView=(ImageView)itemView.findViewById(R.id.image);
             title=(TextView) itemView.findViewById(R.id.title_node);
+            relativeLayout=itemView.findViewById(R.id.relative_layout);
         }
     }
 
@@ -71,7 +67,21 @@ public class adapter extends RecyclerView.Adapter<adapter.viewholder>implements 
         //viewholder.textView.setText(n.getText());
         viewholder.title.setText(n.getTitle());
         Picasso.with(viewholder.imageView.getContext()).load(n.getImage_url()).resize(125,150).into(viewholder.imageView);
+
+
+        viewholder.relativeLayout
+                .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(view.getContext(),"this is a toast", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+    });
     }
+
 
     @Override
     public int getItemCount() {
---
+++ app/src/main/java/com/example/hosel_app/notification_detail.java
@@ -0,0 +1,13 @@
+package com.example.hosel_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class notification_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_detail);
+    }
+}
